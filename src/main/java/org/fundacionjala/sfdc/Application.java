729bb7a7e9f11076bd79225fca41fb53ba629fec
edgarquispe <edgartron32@gmail.com>
2019-11-02 20:53:53 -0400

    Base core project with account creation example

--- src/main/java/org/fundacionjala/sfdc/Application.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.sfdc;
-
-/**
- * @author Benjamin Huanca on 10/31/2019.
- * @version 1.0
- */
-public class Application {
-}
---
+++ src/main/java/org/fundacionjala/sfdc/RequestSpecFactory.java
@@ -0,0 +1,50 @@
+package org.fundacionjala.sfdc;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public final class RequestSpecFactory {
+
+    private static final Environment ENV = Environment.getInstance();
+
+    private RequestSpecFactory() {
+    }
+
+    private static RequestSpecification getRequestSpecSFDC(final String account) {
+        Response response = RestAssured.given()
+                .param("grant_type", "password")
+                .param("client_id", ENV.getValue(String.format("sfdc.credentials.%s.clientId", account)))
+                .param("client_secret", ENV.getValue(String.format("sfdc.credentials.%s.clientSecret", account)))
+                .param("username", ENV.getValue(String.format("sfdc.credentials.%s.userName", account)))
+                .param("password", ENV.getValue(String.format("sfdc.credentials.%s.password", account))
+                        .concat(ENV.getValue(String.format("sfdc.credentials.%s.securityToken", account))))
+                .when()
+                .post("https://login.salesforce.com/services/oauth2/token");
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(response.jsonPath().getString("instance_url").concat("/services/data/v39.0"))
+                .addHeader("Authorization", "Bearer " + response.jsonPath().getString("access_token"))
+                .build();
+        return requestSpecification
+                .log().method()
+                .log().uri()
+                .log().params()
+                .log().body();
+    }
+
+    private static Map<String, Supplier<RequestSpecification>> getRequestSpecMap(final String account) {
+        Map<String, Supplier<RequestSpecification>> requestSpecMap = new HashMap<>();
+        requestSpecMap.put("sfdc", () -> getRequestSpecSFDC(account));
+        return requestSpecMap;
+    }
+
+    public static RequestSpecification getRequestSpec(final String serviceName, final String account) {
+        return getRequestSpecMap(account).get(serviceName).get();
+    }
+}
