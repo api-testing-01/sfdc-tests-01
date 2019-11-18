f489deb8efbc967fbcb0f37c59b312a7f328f7ec
Rosario Falconi <bruxita2312@gmail.com>
2019-11-17 23:15:39 -0400

    Adding groups features files

--- src/main/java/org/fundacionjala/sfdc/core/cucumber/hooks/CommonHook.java
+++ src/main/java/org/fundacionjala/sfdc/core/cucumber/hooks/CommonHook.java
@@ -2,6 +2,7 @@ package org.fundacionjala.sfdc.core.cucumber.hooks;
 
 import io.cucumber.java.After;
 import io.restassured.specification.RequestSpecification;
+import org.fundacionjala.sfdc.core.JsonHelper;
 import org.fundacionjala.sfdc.core.ScenarioContext;
 import org.fundacionjala.sfdc.core.api.RequestManager;
 
@@ -22,5 +23,17 @@ public class CommonHook {
         for (String endpoint : endpoints) {
             RequestManager.delete(requestSpec, endpoint);
         }
+    }
+    @After(value = "@cleanGroups")
+    public void afterGroupsScenario() {
+        System.out.println("\n Beginning afterGroupsScenario");
+        RequestSpecification requestSpec = (RequestSpecification) context.get("REQUEST_SPEC");
+        String endpoint = context.get("LAST_ENDPOINT").toString();
+        System.out.println("\n REQUEST_SPEC  :: " + context.get("REQUEST_SPEC").toString());
+        System.out.println("\n G :: " + context.get("g").getClass().toString());
+        System.out.println("\n Endpoint :: " + endpoint);
+        System.out.println("\n Response :: "+ context.get("LAST_RESPONSE").toString());
+        RequestManager.delete(requestSpec, endpoint);
+        System.out.println("\nEnd afterGroupsScenario");
     }
 }
